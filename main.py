dc58006829f88e6ed9469d5f2d508c7e9ff205b1
Ginotuch <6973573+Ginotuch@users.noreply.github.com>
2018-01-19 14:54:38 +1300

    Finished download info extraction

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ A: "I made this for my setup, if you want to add support for other setups, feel 
 """
 
 from json import loads
+
 import requests
 
 
@@ -16,6 +17,17 @@ def main():
 
 
 class SR:  # SR for Sonarr or Radarr
+    class Item:
+        def __init__(self, path, item_id):
+            self.path = path
+            self.item_id = item_id
+
+        def __repr__(self):
+            return "ID:{} PATH:\"{}\"".format(self.item_id, self.path)
+
+        def __str__(self):
+            return "ID:{} PATH:{}".format(self.item_id, self.path)
+
     def __init__(self, url, username, password):
         self.clear_url = url.replace("http://", "").replace("https://", "").replace("/", "").replace("sonarr", "")
         self.username = username
@@ -24,4 +36,17 @@ class SR:  # SR for Sonarr or Radarr
         self.api_key = requests.get(self.http_pw_url, stream=True, timeout=10).text.split("ApiKey     : '")[1].split("'")[0]
 
     def get_completed(self):
-        r = requests.get("http://{}:{}@")
+        r = requests.get(self.http_pw_url + "/api/queue?apikey=" + self.api_key)
+        rdic = loads(r.text)
+        items = []
+        for x in rdic:
+            if x['status'] != "Completed" or len(x['statusMessages']) != 1:  # Makes sure only one error
+                continue
+            if len(x['statusMessages'][0]['messages']) != 1:  # If more than one issue may not be fake
+                continue
+            if "No files found are eligible for import in" not in x['statusMessages'][0]['messages'][0]:
+                continue
+            items.append(
+                SR.Item(x['statusMessages'][0]['messages'][0].replace("No files found are eligible for import in ", ""),
+                        x['id']))
+        return items
